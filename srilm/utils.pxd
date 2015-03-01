8ea666c1c5d0dbf1fd5ff19629b1b88f3cec5685
Yi Su <nuance1979@hotmail.com>
2015-03-01 10:21:57 -0500

    exposed more utility functions

--- srilm/utils.pxd
+++ srilm/utils.pxd
@@ -12,7 +12,7 @@ cdef extern from "Prob.h":
     ctypedef double Prob
     cdef Prob LogPtoProb(LogP2 prob)
     cdef LogP ProbToLogP(Prob prob)
-    cdef LogP2 MixLogP(LogP2 prob1, LogP2 prob2, double lambda0)
+    cdef LogP2 MixLogP(LogP2 prob1, LogP2 prob2, double lambda1)
     cdef LogP2 AddLogP(LogP2 x, LogP2 y)
     cdef LogP2 SubLogP(LogP2 x, LogP2 y)
     cdef LogP2 weightLogP(double weight, LogP2 prob)
--- srilm/utils.pyx
+++ srilm/utils.pyx
@@ -6,19 +6,38 @@ from cython.operator cimport dereference as deref
 from stats cimport Stats
 
 def rand_seed(long seed):
+    """Set the seed of the random number generator"""
     srand48(seed)
 
 def logprob_to_prob(LogP2 logprob):
+    """Convert a base-10 log probability to a probability"""
     return LogPtoProb(logprob)
 
 def prob_to_logprob(Prob prob):
+    """Convert a probability to a base-10 log probability"""
     return ProbToLogP(prob)
 
 def add_logprob(LogP2 x, LogP2 y):
+    """Add two base-10 log probabilities"""
     return AddLogP(x, y)
 
+def sub_logprob(LogP2 x, LogP2 y):
+    """Substract base-10 log probability y from x""" 
+    return SubLogP(x, y)
+
+def mix_logprob(LogP2 prob1, LogP2 prob2, double lambda1):
+    """Compute the interpolation of two base-10 log probabilites
+
+    Return the equivalent of 
+    prob_to_logprob(lambda1 * logprob_to_prob(prob1) + (1-lambda1) * logprob_to_prob(prob2))
+    """
+    return MixLogP(prob1, prob2, lambda1)
+
 def train_class(Stats ts, unsigned num_class, const char *out_classes, const char *out_class_counts, method = 'inc', exclude_list = ['<s>', '</s>']):
-    """Train an agglomerative clustering, aka, Brown clustering from a word bigram Stats"""
+    """Train an agglomerative clustering, aka, Brown clustering from a word bigram Stats
+
+    Write out class definition and class bigram counts to files.
+    """
     if method not in ['full', 'inc']:
         raise ValueError('Invalid classing method; expect "full" or "inc"')
     if ts.order < 2:
