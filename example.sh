6dc47f84e13229cd9873eb1b37aa1655ce4596b6
Yi Su <nuance1979@hotmail.com>
2015-03-03 19:39:17 -0500

    removed SimpleClassLm from example.py and example.sh

--- example.sh
+++ example.sh
@@ -4,7 +4,7 @@ MT=$(../sbin/machine-type)
 BIN=../bin/$MT
 
 if [[ $# -lt 3 ]]; then
-    echo "Usage: $0 order vocab train heldout test [classes class_counts]"
+    echo "Usage: $0 order vocab train heldout test"
     exit
 fi
 
@@ -13,10 +13,7 @@ VOCAB=$2
 TRAIN=$3
 HELDOUT=$4
 TEST=$5
-CLASSES=$6
-CLASSCOUNTS=$7
 
-TMP=$(mktemp)
 echo "Ngram LM with Good-Turing discount:"
 $BIN/ngram-count -order $ORDER -vocab $VOCAB -unk -text $TRAIN -lm - \
     | $BIN/ngram -order $ORDER -vocab $VOCAB -unk -lm - -ppl $TEST
@@ -33,6 +30,7 @@ echo "Ngram LM with Chen-Goodman discount:"
 $BIN/ngram-count -order $ORDER -vocab $VOCAB -unk -text $TRAIN -lm - -interpolate -kndiscount $args \
     | $BIN/ngram -order $ORDER -vocab $VOCAB -unk -lm - -ppl $TEST
 echo "Ngram LM with Jelinek-Mercer smoothing:"
+TMP=$(mktemp)
 COUNTS=$(mktemp)
 $BIN/ngram-count -order $ORDER -vocab $VOCAB -unk -text $TRAIN -write $COUNTS
 $BIN/ngram-count -vocab $VOCAB -unk -write-vocab $TMP
@@ -50,12 +48,9 @@ vocabsize $VSIZE
 totalcount $TCOUNT
 counts $COUNTS
 EOF
-$BIN/ngram-count -order $ORDER -vocab $VOCAB -unk -text $HELDOUT -init-lm $TMP1 -count-lm -lm - \
+$BIN/ngram-count -order $ORDER -vocab $VOCAB -unk -text $HELDOUT -init-lm $TMP -count-lm -lm - \
     | $BIN/ngram -order $ORDER -vocab $VOCAB -unk -lm - -ppl $TEST -count-lm
 echo "MaxEnt LM:"
 $BIN/ngram-count -order $ORDER -vocab $VOCAB -unk -text $TRAIN -maxent -lm - \
     | $BIN/ngram -order $ORDER -vocab $VOCAB -unk -lm - -ppl $TEST -maxent
-echo "Simple bi-gram class LM:"
-$BIN/ngram-count -order 2 -unk -read $CLASSCOUNTS -lm - $args \
-    | $BIN/ngram -order 2 -vocab $VOCAB -unk -lm - -ppl $TEST -classes $CLASSES -simple-classes
 rm -f $TMP $COUNTS
